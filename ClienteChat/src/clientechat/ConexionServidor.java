5a0fbc6bb933d287e145aab68c13ac5eb6b5fd48
jfrodriguezaguilar <74997742+jfrodriguezaguilar@users.noreply.github.com>
2022-05-31 11:56:00 -0500

    se agrega el proyecto

---
+++ ClienteChat/src/clientechat/ConexionServidor.java
@@ -0,0 +1,54 @@
+
+package clientechat;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import javax.swing.JTextField;
+import org.apache.log4j.Logger;
+
+/**
+ * Esta clase gestiona el envio de datos entre el cliente y el servidor.
+ */
+public class ConexionServidor implements ActionListener {
+    
+    private Logger log = Logger.getLogger(ConexionServidor.class);
+    private Socket socket; 
+    private JTextField tfMensaje;
+    private String usuario;
+    private DataOutputStream salidaDatos;
+    
+    public ConexionServidor(Socket socket, JTextField tfMensaje, String usuario) {
+        this.setSocket(socket);
+        this.tfMensaje = tfMensaje;
+        this.usuario = usuario;
+        try {
+            this.salidaDatos = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            log.error("Error al crear el stream de salida : " + ex.getMessage());
+        } catch (NullPointerException ex) {
+            log.error("El socket no se creo correctamente. ");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            salidaDatos.writeUTF(usuario + ": " + tfMensaje.getText() );
+            tfMensaje.setText("");
+        } catch (IOException ex) {
+            log.error("Error al intentar enviar un mensaje: " + ex.getMessage());
+        }
+    }
+
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+
+}
